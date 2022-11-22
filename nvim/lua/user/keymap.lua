e33f46e5a3cd5e2bbc529e3efaf85c3ea9d87c2a
Hello Jonas <hellojonashp@gmail.com>
2022-11-22 23:24:39 +0200

    map window splits

--- nvim/lua/user/keymap.lua
+++ nvim/lua/user/keymap.lua
@@ -1,6 +1,19 @@
 local keymap = vim.keymap.set
 
+local opts = { noremap = true }
+
 -- Buffer navigation
-vim.keymap.set('n', '<leader>bc', '<cmd>bdelete<CR>', {})
-vim.keymap.set('n', '<S-l>', '<cmd>bnext<CR>', {})
-vim.keymap.set('n', '<S-h>', '<cmd>bprevious<CR>', {})
+vim.keymap.set('n', '<leader>bc', '<cmd>bdelete<CR>', opts)
+vim.keymap.set('n', '<S-l>', '<cmd>bnext<CR>', opts)
+vim.keymap.set('n', '<S-h>', '<cmd>bprevious<CR>', opts)
+
+
+-- window navigation
+vim.keymap.set('n', '<C-k>', '<C-w>k', opts)
+vim.keymap.set('n', '<C-j>', '<C-w>j', opts)
+vim.keymap.set('n', '<C-l>', '<C-w>l', opts)
+vim.keymap.set('n', '<C-h>', '<C-w>h', opts)
+
+vim.keymap.set('n', '<leader>sr', ':vsplit<CR>', opts)
+vim.keymap.set('n', '<leader>sb', ':split<CR>', opts)
+vim.keymap.set('n', '<leader>sc', '<c-w>c', opts)
